73515f0340803c63398e6e51534979fcb227f58f
nympheastudio <perodo@gmail.com>
2022-05-23 23:43:16 +0200

    clean programme

--- app/screens/Programme.js
+++ app/screens/Programme.js
@@ -2,34 +2,16 @@ import React, { useEffect, useState, useRef } from "react";
 import {
   Image,
   Text,
-  TextInput,
-  Linking,
-  DrawerContentScrollView,
+  
   View,
-  StyleSheet,
-  ScrollViewButton,
-  ScrollView,
-  Button,
+  
   FlatList,
-  TouchableOpacity,
-  TouchableHighlight,
-  Modal,
-  Pressable,
-  TouchableWithoutFeedback,
+  
   RefreshControl,
-  Share
+  
 } from "react-native";
 import axios from "axios";
 
-import { ActivityIndicator, ToastAndroid } from "react-native";
-import { Image as ImgLazy, Icon } from "react-native-elements";
-
-import { Detail } from "./Detail";
-import { Card } from "react-native-paper";
-import styles from "../config/styles/StyleGeneral";
-import WebView from "react-native-webview";
-
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { ShopContext } from "../store/ShopContext";
 import { FavorisContext } from "../store/FavorisContext";
@@ -48,25 +30,25 @@ const url_programme = baseUrl + "api2022.php?a=1";
 export default function Programme({ navigation }) {
   const { state, dispatch } = React.useContext(StoreContext);
   const context = React.useContext(ShopContext);
-
+  
   // initialize data state variable as an empty array
-
+  
   const [data, setData] = useState([]);
   const [input, setInput] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
   const [visible, setVisible] = useState(true);
-
-
+  
+  
   const [filter, setFilter] = useState("all");
   const [categories, setCategories] = useState("all");
   const [searchText, setSearchText] = useState();
-
+  
   const [refreshing, setRefreshing] = useState(false);
-
+  
   const [isLoading, setIsLoading] = useState(false);
-
-
-
+  
+  
+  
   useEffect(() => {
     setIsLoading(true);
     axios.get(url_programme).then((response) => {
@@ -75,54 +57,46 @@ export default function Programme({ navigation }) {
       setIsLoading(false);
     });
   }, []);
-
-
+  
+  
   /*
   useEffect(() => {
     console.log("context cart length from Programme : :" + context.cart.length);
   }, []);
-*/
-
-
-
-
-
+  */
+  
+  
+  
+  
+  
   const filteredData = searchText
-    ? state.programme.filter(
-        (x) =>
-          x.description.toLowerCase().includes(searchText.toLowerCase()) ||
-          x.nom.toLowerCase().includes(searchText.toLowerCase()) ||
-          x.lieu.toLowerCase().includes(searchText.toLowerCase())
-      )
+  ? state.programme.filter(
+    (x) =>
+    x.description.toLowerCase().includes(searchText.toLowerCase()) ||
+    x.nom.toLowerCase().includes(searchText.toLowerCase()) ||
+    x.lieu.toLowerCase().includes(searchText.toLowerCase())
+    )
     : state.programme;
-
-  let item_nom = "";
-  const renderHeader = () => {
-    return <Text style={{
-      marginHorizontal: 0,
-      marginVertical: 100,
-    fontSize: 22, fontWeight: 'bold'}}>Header</Text>;
-  };
-
-  
-  
-  return (
-    <View>
-      {isLoading && <Loader />}
-
+    
+ 
       
-      <ProgrammeHeader />
-
-      <FlatList
-
-    //  ListHeaderComponent={() => { return <ProgrammeHeader />}}
-
+      return (
+        <View>
+        {isLoading && <Loader />}
+        
+        
+        <ProgrammeHeader />
+        
+        <FlatList
+        
+        //  ListHeaderComponent={() => { return <ProgrammeHeader />}}
+        
         data={filteredData}
         removeClippedSubviews={true}
         
         maxToRenderPerBatch={5}
         initialNumToRender={5}
-
+        
         onEndReached={({ distanceFromEnd }) => {
           if (distanceFromEnd < 0) return;
         }}
@@ -130,30 +104,26 @@ export default function Programme({ navigation }) {
           // console.log("index", index)
           return index.toString();
         }}
-
         
-
+        
+        
         renderItem={({ item }) => {
-         // return renderData(item);
-         return <ProgrammeCard item={item} />
+          // return renderData(item);
+          return <ProgrammeCard item={item} />
         }}
-
-
+        
+        
         refreshControl={
           <RefreshControl
-            refreshing={refreshing}
-            onRefresh={() =>
-              dispatch({ type: "addData", payload: filteredData })
-            } />
+          refreshing={refreshing}
+          onRefresh={() =>
+            dispatch({ type: "addData", payload: filteredData })
+          } />
         }
-
-
-      />
-      {/* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< FIN LISTE PROGRAMME APPEL */}
-
+        
+        
+        />
+        </View>
+        );
+      }
       
-
-      {/* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< FIN FICHE PROGRAMME */}
-    </View>
-  );
-}
