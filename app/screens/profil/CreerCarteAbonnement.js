3ad61171dc03aea1e9929e9a432765ac24abc917
nympheastudio <perodo@gmail.com>
2022-06-08 11:40:42 +0200

    change asyncstorage for expostorage

--- app/screens/profil/CreerCarteAbonnement.js
+++ app/screens/profil/CreerCarteAbonnement.js
@@ -18,64 +18,83 @@ import {
   TouchableWithoutFeedback,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Storage } from 'expo-storage';
+
+
 import {CartesAbonnementContext} from "../../store/storeCartesAbonnement";
 import { NavigationContainer } from "@react-navigation/native";
 
+import RNPickerSelect from "react-native-picker-select";
 import styles from "../../config/styles/StyleGeneral";
 
 import * as ImagePicker from 'expo-image-picker';
 
 export default function CreerCarteAbonnement({ navigation }) {
-
-
+  
+  
   const { stateCartesAbonnement, dispatchCartesAbonnement } = React.useContext( CartesAbonnementContext );
-
+  
   const [Nom, setNom] = useState("");
   const [Prenom, setPrenom] = useState("");
   const [Adresse, setAdresse] = useState("");
   const [CodePostal, setCodePostal] = useState("");
   const [Ville, setVille] = useState("");
-  const [Pays, setPays] = useState("");
+  const [Pays, setPays] = useState("France");
   const [Telephone, setTelephone] = useState("");
   const [livraison, setLivraison] = useState("");
   const [photo, setPhoto] = useState("");
   const [image, setImage] = useState(null);
+ const [statut, setStatut] = useState("");
+ const [numero_carte, setNumeroCarte] = useState("");
+ const [code_promo, setCodePromo] = useState("");
+ const [structure, setStructure] = useState("");
+ const [birthday, setBirthday] = useState("");
+ const [card_type_id, setCard_type_id] = useState("");
+ const [skip_step, setSkip_step] = useState("");
+ const [transport_card, setTransport_card] = useState("");
+ const [culture_card, setCulture_card] = useState("");
+ const [partner_festival, setPartner_festival] = useState("");
+ const [reduced_card, setReduced_card] = useState("");
 
+  
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
+    //  let result = await ImagePicker.launchImageLibraryAsync({
+    let result = await ImagePicker.launchCameraAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [3, 3],
       quality: 0.3,
       base64: true,
-
+      
     });
-
+    
     console.log(result);
-
+    
     if (!result.cancelled) {
       setImage( result.uri);
       setPhoto(result.base64);
       //'data:image/jpeg;base64,'
     }
   };
-
-
+  
+  
   const addCarteAbonnement = async() => {
-
-   // AsyncStorage.setItem("CartesAbonnement", JSON.stringify(stateCartesAbonnement.Cartes));
-
-   
+    
+    // AsyncStorage.setItem("CartesAbonnement", JSON.stringify(stateCartesAbonnement.Cartes));
+    
+    
     console.log("CartesAbonnement saved");
-
-
+    
+    
     const carteAbonnement = {
+
+      "fes_id": 22,
       "id": stateCartesAbonnement.Cartes.length + 1,
-      "statut" : "brouillon",
-      "numero_carte" : "123456789",
-      "code_promo": "",
-      "structure": "",
+      "statut" : statut,
+      "numero_carte" : numero_carte,
+      "code_promo": code_promo,
+      "structure": structure,
       "nom" : Nom,
       "prenom" : Prenom,
       "adresse" : Adresse,
@@ -84,182 +103,255 @@ export default function CreerCarteAbonnement({ navigation }) {
       "telephone" : Telephone,
       "pays" : Pays,
       "livraison" : livraison,
-      "photo": photo
+      "photo": photo,
+
+      "birthday" : birthday,
+      "card_type_id" : card_type_id,
+      "skip_step" : skip_step,
+      
+      "transport_card" : transport_card,
+      "culture_card" :culture_card,
+      "partner_festival" : partner_festival,
+      "reduced_card" :reduced_card
     }
     
-      dispatchCartesAbonnement({type: "ADD_CARTE_ABONNEMENT", payload: carteAbonnement})
+    dispatchCartesAbonnement({type: "ADD_CARTE_ABONNEMENT", payload: carteAbonnement})
     
- 
+    
       
-   
     
-
+    
+    
     navigation.navigate("CartesAbonnement");
   }
-
+  
   useEffect(() => {
-
-
-//add values to async storage
-
-
-
-
+    
+    
+    //add values to async storage
+    
+    
+    
+    
   }, [stateCartesAbonnement.Cartes]);
-  /*
-    JSON.stringify(stateCartesAbonnement.Cartes))
-  }, [stateCartesAbonnement.Cartes]);
+  
+  
+  
+/*
+Le fonctionnement serait le suivant :
+
+CARTES PANIER  Obtention pour obtenir les cartes en cours
+
+CARTES PANIER Choix carte (Qui en fonction des critères , détermine la carte à créer : normale, 12/15, transport, tarif réduit, culture et le tarif associé)
+
+CARTES PANIER Ajout carte pour ajouter la carte au panier en cours
+
+CARTES PANIER Validation  pour verrouiller la panier et interdire l’ajout de nouvelles cartes avant paiement
+
+Après il faudrait basculer sur la page de paiement à créer
+
+ 
+
+CARTES PANIER Annulation permet de déverrouiller le panier et ajouter à nouveau des cartes
+
 */
 
-//useEffect to save new carteAbonnement in AsyncStorage
-/*
-  useEffect(() => {
-    AsyncStorage.setItem("CartesAbonnement", JSON.stringify(stateCartesAbonnement.Cartes));
-  }, [stateCartesAbonnement.Cartes]);
-
-  */
 
 
+
+
+return (
   
+  <View
+  
+  style={{
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    top: "0%",
+  }}
+  
+  key="creerCartesAbonnement"
+  >
+  <ScrollView>
+  <Text style={{ fontSize: 20 }}>Créer une carte d'abonnement</Text>
+  <Text style={{ fontSize: 20 }}>Type de carte</Text>
+  
+  <RNPickerSelect
+  
+  
+  value={card_type_id}
+  onValueChange={(value) =>
+    {
+      setCard_type_id(value);
+      //setFieldValue("type_public", value)
+      
+    }
+  }
+  
+  items={[
+    { label: "Normale", value: "Normale," },
+    { label: "12/15", value: "12/15" },
+    { label: "transport", value: "transport" },
+    { label: "tarif réduit", value: "tarif réduit" },
+    { label: "culture", value: "culture" },
+  ]}
+  />
 
-
-  return (
-
-    <View
-
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        top: "0%",
-      }}
-
-      key="creerCartesAbonnement"
-    >
-      <ScrollView>
-      <Text style={{ fontSize: 20 }}>Créer une carte d'abonnement</Text>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 20, height: 20 }} />}
+  <Text style={{ fontSize: 20 }}>Nom</Text>
+  <TextInput
+  
+  style={{
+    height: 40,
+    width: "80%",
+    borderColor: "gray",
+    borderWidth: 1,
+    margin: 10,
+  }}
+  onChangeText={(text) => setNom(text)}
+  value={Nom}
+  />
+  
+  <Text style={{ fontSize: 20 }}>Prenom</Text>
+  <TextInput
+  
+  style={{
+    height: 40,
+    width: "80%",
+    borderColor: "gray",
+    borderWidth: 1,
+    margin: 10,
+  }}
+  onChangeText={(text) => setPrenom(text)}
+  value={Prenom}
+  />
+  
+  <Text style={{ fontSize: 20 }}>Adresse</Text>
+  <TextInput
+  style={{
+    height: 40,
+    width: "80%",
+    borderColor: "gray",
+    borderWidth: 1,
+    margin: 10,
+  }}
+  onChangeText={(text) => setAdresse(text)}
+  value={Adresse}
+  />
+  
+  <Text style={{ fontSize: 20 }}>Code postal</Text>
+  <TextInput
+  style={{
+    height: 40,
+    width: "80%",
+    borderColor: "gray",
+    borderWidth: 1,
+    margin: 10,
+  }}
+  onChangeText={(text) => setCodePostal(text)}
+  value={CodePostal}
+  />
+  
+  <Text style={{ fontSize: 20 }}>Ville</Text>
+  <TextInput
+  style={{
+    height: 40,
+    width: "80%",
+    borderColor: "gray",
+    borderWidth: 1,
+    margin: 10,
+  }}
+  onChangeText={(text) => setVille(text)}
+  value={Ville}
+  />
+  <Text style={{ fontSize: 20 }}>Telephone</Text>
+  <TextInput
+  keyboardType={'phone-pad'}
+  style={{
+    height: 40,
+    width: "80%",
+    borderColor: "gray",
+    borderWidth: 1,
+    margin: 10,
+  }}
+  onChangeText={(text) => setTelephone(text)}
+  value={Telephone}
+  />
+  <Text style={{ fontSize: 20 }}>Pays</Text>
+  
+  <RNPickerSelect
+  //style={pickerStyle}
+  
+  
+  placeholder={{
+    label: Pays,
+    value: Pays,
+  }}
+  
+  
+  value={Pays}
+  onValueChange={(value) =>
+    {
+      setPays(value);
+      //setFieldValue("type_public", value)
+      
+    }
+  }
+  
+  items={[
+    { label: "France", value: "France" },
+    { label: "Belgique", value: "Belgique" },
+    { label: "USA", value: "USA" },
+    // { label: "Public non francophone", value: "Public non francophone", },
+  ]}
+  />
+  
+  
+  
+  <Text style={{ fontSize: 20 }}>Livraison</Text>
+  
+  <RNPickerSelect
+  //style={pickerStyle}
+  
+  
+  placeholder={{
+    label: livraison,
+    value: livraison,
+  }}
+  
+  
+  value={livraison}
+  onValueChange={(value) =>
+    {
+      setLivraison(value);
+      //setFieldValue("type_public", value)
+      
+    }
+  }
+  
+  items={[
+    { label: "Sur place", value: "Sur place" },
+    { label: "A distance", value: "A distance" },
+    { label: "test", value: "test" },
+    // { label: "Public non francophone", value: "Public non francophone", },
+  ]}
+  />
+  <Button 
+  title="Prendre une photo" onPress={pickImage} />
+  {image && <Image source={{ uri: image }} style={{ width: 20, height: 20 }} />}
+  <Button
+  title="Créer"
+  onPress={() => {
     
-      <Text style={{ fontSize: 20 }}>Nom</Text>
-      <TextInput
-
-        style={{
-          height: 40,
-          width: "80%",
-          borderColor: "gray",
-          borderWidth: 1,
-          margin: 10,
-        }}
-        onChangeText={(text) => setNom(text)}
-        value={Nom}
-      />
-
-      <Text style={{ fontSize: 20 }}>Prenom</Text>
-      <TextInput
-
-        style={{
-          height: 40,
-          width: "80%",
-          borderColor: "gray",
-          borderWidth: 1,
-          margin: 10,
-        }}
-        onChangeText={(text) => setPrenom(text)}
-        value={Prenom}
-      />
-
-      <Text style={{ fontSize: 20 }}>Adresse</Text>
-      <TextInput
-       style={{
-        height: 40,
-        width: "80%",
-        borderColor: "gray",
-        borderWidth: 1,
-        margin: 10,
-      }}
-        onChangeText={(text) => setAdresse(text)}
-        value={Adresse}
-      />
-
-      <Text style={{ fontSize: 20 }}>Code postal</Text>
-      <TextInput
-        style={{
-          height: 40,
-          width: "80%",
-          borderColor: "gray",
-          borderWidth: 1,
-          margin: 10,
-        }}
-        onChangeText={(text) => setCodePostal(text)}
-        value={CodePostal}
-      />
-
-      <Text style={{ fontSize: 20 }}>Ville</Text>
-      <TextInput
-       style={{
-        height: 40,
-        width: "80%",
-        borderColor: "gray",
-        borderWidth: 1,
-        margin: 10,
-      }}
-        onChangeText={(text) => setVille(text)}
-        value={Ville}
-      />
-
-      <Text style={{ fontSize: 20 }}>Pays</Text>
-      <TextInput
-        style={{
-          height: 40,
-          width: "80%",
-          borderColor: "gray",
-          borderWidth: 1,
-          margin: 10,
-        }}
-        onChangeText={(text) => setPays(text)}
-        value={Pays}
-      />
-
-      <Text style={{ fontSize: 20 }}>Telephone</Text>
-      <TextInput
-        style={{
-          height: 40,
-          width: "80%",
-          borderColor: "gray",
-          borderWidth: 1,
-          margin: 10,
-        }}
-        onChangeText={(text) => setTelephone(text)}
-        value={Telephone}
-      />
-
-      <Text style={{ fontSize: 20 }}>Livraison</Text>
-      <TextInput
-       style={{
-        height: 40,
-        width: "80%",
-        borderColor: "gray",
-        borderWidth: 1,
-        margin: 10,
-      }}
-        onChangeText={(text) => setLivraison(text)}
-        value={livraison}
-      />
-
-      <Button
-        title="Créer"
-        onPress={() => {
-
-          addCarteAbonnement(Nom, Prenom, Adresse, CodePostal, Ville, Pays, Telephone, livraison);
-          
-        }}
-      />
-      </ScrollView>
-    </View>
-
-
-
+    addCarteAbonnement();
+    
+  }}
+  />
+  </ScrollView>
+  </View>
+  
+  
+  
   );
- 
+  
 }
--- app/screens/profil/PlacesSpectacles.js
+++ app/screens/profil/PlacesSpectacles.js
@@ -18,20 +18,136 @@ import {
   TouchableWithoutFeedback,
 } from "react-native";
 import axios from "axios";
-
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { StoreContext } from "../../store/store";
+import RNPickerSelect from "react-native-picker-select";
+import {CartesAbonnementContext} from "../../store/storeCartesAbonnement";
+
+import styles from "../../config/styles/StyleGeneral";
+
 
 /*
 {state.isAuthenticated && (
   <Text style={{fontSize: 20}}>Bonjour{state.user.nom}</Text>
   
   )}
+
+
   */
 
 export default function PlacesSpectacles({ navigation }) {
-  const { state, dispatch } = React.useContext(StoreContext);
+ // const { state, dispatch } = React.useContext(StoreContext);
+const [Places, setPlaces] = useState([]);
+const [isLoading, setIsLoading] = useState(true);
+ // var axios = require('axios');
+
+ const getFactures = async() => {
+var data = '{\r\n    fes_id : 21\r\n}';
+
+var config = {
+  method: 'post',
+  url: 'https://api.festivaloffavignon.com/tickets',
+  headers: { 
+    'api-key': '8eq+GmvX;]#.t_h-(nwT68ZXf-{2&Pr8', 
+    'token': 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJvZmYtand0IiwidG9rZW5faWQiOiJjZGVhMzcwMy05OWRiLTQ5MTQtOTExNS1iZDFiNWFkMjM4Y2YiLCJ1c2VyX2lkIjo2ODA4MywiaWF0IjoxNjUyNzE3MzQ3LCJleHAiOjE2NTUzMDkzNDd9.ha7S4Mx_L2keGXHBP_jO6LxA89Z2e0mRmaGlQ0axPuH87PFABpN8iO1EdQRwYRKP_zWgnLhCA3DZQ4yUFgPfuKZtPHOjqBXT1HGUrfR843HMCSZBmjwKyDjP3bLhVcG5yAB42SL73Pm0XdLM9W_rgvmbi2YFRtyQXYLcIaSV3Qg', 
+    'Content-Type': 'text/plain', 
+    'Cookie': '.ASPXANONYMOUS=C5AaxfWW2AEkAAAAMjZlMGI5YzUtMzZhNC00ZmI3LWJhOWUtYjcxMzEwNjJmMWZmJnHD8r3JRaHoMX5AiBpQOd6w5NNFbICO7Y56PMvrWz81'
+  },
+  data : data
+};
+
+
+
+await axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+  setPlaces(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+
+};
+/*
+useEffect(async() => {
+
+await getFactures();
+
+
+}, []);
+
+
+*/
+
+  
+const renderItem = ({ item,i }) => {
+  // console.log(item.photo);
+   //item data
+   /*
+   "id": 1,    
+   "statut" : "brouillon",
+   "numero_carte" : "123456789",
+   "code_promo": "",
+   "structure": "",
+   "nom" : "perodo",
+   "prenom" : "nico",
+   "adresse" : "rue de la paix",
+   "ville" : "paris",
+   "codePostal" : "75000",
+   "telephone" : "0123456789",
+   "pays" : "france",
+   "livraison" : "courrier",
+   "photo": ""
+
+   "fes_id": 22,
+   "card_birthday" : "12/12/1960",
+   "card_type_id" : 1,
+     "skip_step" : false,
+ "transport_card" : true,
+ "culture_card" : true,
+ "partner_festival" : true,
+ "reduced_card" : true
+   */
+   
+   return(
+     <View style={styles.carteAbonnement} key={item.id}>
+     <View style={styles.carteAbonnement_header}>
+     <Text style={styles.carteAbonnement_header_text}>Carte Abonnement</Text>
+     </View>
+     <View style={styles.carteAbonnement_body}>
+     <View style={styles.carteAbonnement_body_left}>
+     
+     <Pressable
+     
+     onPress={() => {
+       
+       deleteCarteAbonnement(item.id);
+       
+     }}
+     ><Text>Supp</Text>
+     </Pressable> 
+     
+     </View>
+     <View style={styles.carteAbonnement_body_right}>
+     <Text style={styles.carteAbonnement_header_text}>{item.nom} {item.prenom}</Text>
+     
+     <Image
+     source={{ uri: 'data:image/jpeg;base64,' + item.photo }}
+     style={{ width: 100, height: 100 }}
+     />
+     
+     </View>
+     
+     </View>
+     </View>
+     
+     );
+     
+   }
+   
+   
+
 
   return (
     <View
@@ -42,7 +158,38 @@ export default function PlacesSpectacles({ navigation }) {
         top: "10%",
       }}
     >
-      <Text style={{ fontSize: 20 }}>PlacesSpectacles</Text>
+     <FlatList
+      data={Places}
+      // extraData={newCartes}
+      renderItem={(item) => renderItem(item)}
+      keyExtractor={(item) => item.id}
+      ItemSeparatorComponent={() => <View style={{ height: 1, backgroundColor: "black" }} />}
+      style={{ width: "100%", height: "100%" }}
+      
+      onEndReached={() => {
+        setIsLoading(true);
+        setTimeout(() => {
+          setIsLoading(false);
+        }
+        , 2000);
+      }
+    }
+    
+    onEndReachedThreshold={0.5}
+    refreshing={isLoading}
+    onRefresh={() => {
+      setIsLoading(true);
+      setTimeout(() => {
+        setIsLoading(false);
+      }
+      , 2000);
+      
+    }
+  }
+  
+  
+  
+  />
     </View>
   );
 }
