c6cca16e43b3cc2e292e647189678ceea28df307
nympheastudio <perodo@gmail.com>
2022-06-17 01:01:01 +0200

    ajout factures et cartes abonnements

--- app/screens/profil/CartesAbonnement.js
+++ app/screens/profil/CartesAbonnement.js
@@ -22,7 +22,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Storage } from 'expo-storage';
 import RNPickerSelect from "react-native-picker-select";
 import {CartesAbonnementContext} from "../../store/storeCartesAbonnement";
-
+import { useDispatch, useSelector } from "react-redux";
 import styles from "../../config/styles/StyleGeneral";
 import { FlatList } from "react-native-gesture-handler";
 
@@ -30,58 +30,48 @@ import { FlatList } from "react-native-gesture-handler";
 export default function CartesAbonnement({ navigation }) {
   
   const { stateCartesAbonnement, dispatchCartesAbonnement } = React.useContext( CartesAbonnementContext );
-  
+  const user = useSelector((state) => state.user);
+
   const [Cartes, setCartes] = useState([]);
+  const [CartesAchetees, setCartesAchetees] = useState({});
   
   const [isLoading, setIsLoading] = useState(true);
   
   
  
-const CartesAchetees = {
-    
- 
-  Cartes: 
+  
+
+      const getCartesAchetees = async () => {
+        let data = '{"fes_id":22}';
+        
+        let config = {
+          method: 'post',
+          url: 'https://api.festivaloffavignon.com/cards/buyed',
+          headers: { 
+            'api-key': '8eq+GmvX;]#.t_h-(nwT68ZXf-{2&Pr8', 
+            'token': user.token },
+          data : data
+        };
+        
+        await axios(config)
+        .then(function (response) {
+          console.log(JSON.stringify(response.data));
+          setCartesAchetees(response.data.cards);
+
       
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        
+      }
+      
+      useEffect(() => { 
+        getCartesAchetees();
+        setIsLoading(false);
+      }
+      , []);
 
-          [ 
-             {
-              "id": 1300,    
-              "statut" : "achetée",
-                  "numero_carte" : "123456789",
-                  "code_promo": "",
-                  "structure": "",
-                  "nom" : "perodo",
-                  "prenom" : "nico",
-                  "adresse" : "rue de la paix",
-                  "ville" : "paris",
-                  "codePostal" : "75000",
-                  "telephone" : "0123456789",
-                  "pays" : "france",
-                  "livraison" : "courrier",
-                  "photo": "data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs="
-              },
-              {
-                  "id": 2333,
-                  "statut" : "achetée",
-                  "numero_carte" : "123456789",
-                  "code_promo": "",
-                  "structure": "",
-                  "nom" : "perodo2",
-                  "prenom" : "nico",
-                  "adresse" : "rue de la paix",
-                  "ville" : "paris",
-                  "codePostal" : "75000",
-                  "telephone" : "0123456789",
-                  "pays" : "france",
-                  "livraison" : "courrier",
-                  "photo": "data:image/gif;base64,R0lGODlhAQABAIAAAMLCwgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
-              },
-          ]
-  
-
-  
-      };
-  
   const deleteCarteAbonnement = async (id) => {
     try {
      // await AsyncStorage.removeItem('CartesAbonnement');
@@ -110,35 +100,70 @@ const CartesAchetees = {
   
   
   
+  const renderCarteAchetee = ({ item,i }) => {
+    // console.log(item.photo);
+     //item data
+              /*
+           "cards":[
+ {
+ "card_key":"W02751LQWO",
+ "card_firstname":"Céline",
+ "card_lastname":"Hauguel",
+ "card_longname":"Céline Hauguel",
+ "card_photo":"https://www.festivaloffavignon.com/resources/OFF/cartes/photos/2022/189244-5102.png",
+ "card_price":1600,
+ "card_type_id":1,
+ "card_type":"Carte d'abonnement public"
+ },
+ */
+     
+     
+     return(
+       <View style={styles.carteAbonnement} key={item.card_key}>
+       <View style={styles.carteAbonnement_header}>
+       <Text style={styles.carteAbonnement_header_text}>{item.card_type}</Text>
+       </View>
+       <View style={styles.carteAbonnement_body}>
+       <View style={styles.carteAbonnement_body_left}>
+       <Text>carte n° {item.card_key}</Text>
+       <Text>prix {item.card_price/100} Eur</Text>
+      
+       
+       </View>
+       <View style={styles.carteAbonnement_body_right}>
+       <Text style={styles.carteAbonnement_header_text}>{item.card_longname}</Text>
+       
+       <Image
+       source={{ uri:  item.card_photo }}
+       style={{ width: 100, height: 100 }}
+       />
+       
+       </View>
+       
+       </View>
+       </View>
+       
+       );
+       
+     }
   
   const renderItem = ({ item,i }) => {
    // console.log(item.photo);
     //item data
-    /*
-    "id": 1,    
-    "statut" : "brouillon",
-    "numero_carte" : "123456789",
-    "code_promo": "",
-    "structure": "",
-    "nom" : "perodo",
-    "prenom" : "nico",
-    "adresse" : "rue de la paix",
-    "ville" : "paris",
-    "codePostal" : "75000",
-    "telephone" : "0123456789",
-    "pays" : "france",
-    "livraison" : "courrier",
-    "photo": ""
-
-    "fes_id": 22,
-    "card_birthday" : "12/12/1960",
-    "card_type_id" : 1,
-    	"skip_step" : false,
-	"transport_card" : true,
-	"culture_card" : true,
-	"partner_festival" : true,
-	"reduced_card" : true
-    */
+             /*
+          "cards":[
+{
+"card_key":"W02751LQWO",
+"card_firstname":"Céline",
+"card_lastname":"Hauguel",
+"card_longname":"Céline Hauguel",
+"card_photo":"https://www.festivaloffavignon.com/resources/OFF/cartes/photos/2022/189244-5102.png",
+"card_price":1600,
+"card_type_id":1,
+"card_type":"Carte d'abonnement public"
+},
+*/
+    
     
     return(
       <View style={styles.carteAbonnement} key={item.id}>
@@ -194,10 +219,10 @@ const CartesAchetees = {
       >
       <Text style={{ fontSize: 20 }}>Mes cartes achetés</Text>
       <FlatList
-      data={CartesAchetees.Cartes}
+      data={CartesAchetees}
       // extraData={newCartes}
-      renderItem={(item) => renderItem(item)}
-      keyExtractor={(item) => item.id}
+      renderItem={(item) => renderCarteAchetee(item)}
+      keyExtractor={(item) => item.card_key}
       ItemSeparatorComponent={() => <View style={{ height: 1, backgroundColor: "black" }} />}
       style={{ width: "100%", height: "100%" }}
       
--- app/screens/profil/Factures.js
+++ app/screens/profil/Factures.js
@@ -7,21 +7,14 @@ import {
   TextInput,
   DrawerContentScrollView,
   View,
-  StyleSheet,
-  ScrollViewButton,
-  ScrollView,
-  Button,
-  Switch,
-  TouchableOpacity,
-  Modal,
+  Linking,
   Pressable,
   TouchableWithoutFeedback,
 } from "react-native";
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import RNPickerSelect from "react-native-picker-select";
-import {CartesAbonnementContext} from "../../store/storeCartesAbonnement";
-
+import { useDispatch, useSelector } from "react-redux";
 import styles from "../../config/styles/StyleGeneral";
 import { FlatList } from "react-native-gesture-handler";
 
@@ -34,134 +27,85 @@ import { FlatList } from "react-native-gesture-handler";
   */
 
 export default function Factures({ navigation }) {
- 
 
+  const user = useSelector((state) => state.user);
   const [isLoading, setIsLoading] = useState(true);
+  const[invoices, setInvoices] = useState({});
   
   
- 
-const Factures = {
-    
- 
-  Tickets: 
-      
-
-          [ 
-             {
-              "id": 1,    
-              "statut" : "achetée",
-                  "numero_carte" : "123456789",
-                  "code_promo": "",
-                  "structure": "",
-                  "nom" : "perodo",
-                  "prenom" : "nico",
-                  "adresse" : "rue de la paix",
-                  "ville" : "paris",
-                  "codePostal" : "75000",
-                  "telephone" : "0123456789",
-                  "pays" : "france",
-                  "livraison" : "courrier",
-                 
-              },
-              {
-                  "id": 2,
-                  "statut" : "achetée",
-                  "numero_carte" : "123456789",
-                  "code_promo": "",
-                  "structure": "",
-                  "nom" : "perodo2",
-                  "prenom" : "nico",
-                  "adresse" : "rue de la paix",
-                  "ville" : "paris",
-                  "codePostal" : "75000",
-                  "telephone" : "0123456789",
-                  "pays" : "france",
-                  "livraison" : "courrier",
-                
-              },
-          ],
-  Cartes: 
-      
-
-          [ 
-             {
-              "id": 1,    
-              "statut" : "achetée",
-                  "numero_carte" : "123456789",
-                  "code_promo": "",
-                  "structure": "",
-                  "nom" : "perodo",
-                  "prenom" : "nico",
-                  "adresse" : "rue de la paix",
-                  "ville" : "paris",
-                  "codePostal" : "75000",
-                  "telephone" : "0123456789",
-                  "pays" : "france",
-                  "livraison" : "courrier",
-                 
-              },
-              {
-                  "id": 2,
-                  "statut" : "achetée",
-                  "numero_carte" : "123456789",
-                  "code_promo": "",
-                  "structure": "",
-                  "nom" : "perodo2",
-                  "prenom" : "nico",
-                  "adresse" : "rue de la paix",
-                  "ville" : "paris",
-                  "codePostal" : "75000",
-                  "telephone" : "0123456789",
-                  "pays" : "france",
-                  "livraison" : "courrier",
-                
-              },
-          ]
+ const getInvoices = async () => {
+  let data = '';
   
-
+  let config = {
+    method: 'post',
+    url: 'https://api.festivaloffavignon.com/profile/invoices',
+    headers: { 
+      'api-key': '8eq+GmvX;]#.t_h-(nwT68ZXf-{2&Pr8', 
+      'token': user.token },
+    data : data
+  };
   
-      };
+  await axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data.invoices));
+    setInvoices(response.data.invoices);
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+  
+}
+
+useEffect(() => { 
+  getInvoices();
+  setIsLoading(false);
+}
+, []);
 
 
-const handleDownload = ({ item }) => {
-  console.log("download" + item.id);
+const handleDownload = async( url ) => {
+  await axios
+  .get("https://api.festivaloffavignon.com/cms/login", {
+  headers: {
+    "api-key": "8eq+GmvX;]#.t_h-(nwT68ZXf-{2&Pr8",
+    token: user.token,
+  },
+})
+.then((response) => {
+  console.log("get cms/login" );
+  console.log(response.data);
+  console.log("download invoice : " + url);
+  Linking.openURL(url);
+})
+
+.catch((error) => {
+  console.log(error);
+  
+});
+
+
 };
 
 
   
   const renderItem = ({ item,i }) => {
-   // console.log(item.photo);
-    //item data
-    /*
-    "id": 1,    
-    "statut" : "brouillon",
-    "numero_carte" : "123456789",
-    "code_promo": "",
-    "structure": "",
-    "nom" : "perodo",
-    "prenom" : "nico",
-    "adresse" : "rue de la paix",
-    "ville" : "paris",
-    "codePostal" : "75000",
-    "telephone" : "0123456789",
-    "pays" : "france",
-    "livraison" : "courrier",
-    "photo": ""
-
-    "fes_id": 22,
-    "card_birthday" : "12/12/1960",
-    "card_type_id" : 1,
-    	"skip_step" : false,
-	"transport_card" : true,
-	"culture_card" : true,
-	"partner_festival" : true,
-	"reduced_card" : true
-    */
+    /*"invoices":[
+{
+"fes_id":22,
+"date":"/Date(1655287157783+0200)/",
+"date_string":"2022-06-15T11:59:17.7830000",
+"invoice_number":"CA-F2022-025343",
+"url":"https://www.festivaloffavignon.com/mon-compte/impression-facture-85272"
+}
+]
+}
+*/
     
     return(
-      <View style={styles.carteAbonnement} key={item.id}>
+      <View style={styles.carteAbonnement} key={item.invoice_number}>
       <View style={styles.carteAbonnement_header}>
-      <Text style={styles.carteAbonnement_header_text}>Facture N° {item.id}</Text>
+      <Text style={styles.carteAbonnement_header_text}>Facture N° {item.invoice_number}</Text>
       </View>
       <View style={styles.carteAbonnement_body}>
       <View style={styles.carteAbonnement_body_left}>
@@ -170,7 +114,7 @@ const handleDownload = ({ item }) => {
       
       onPress={() => {
         
-        handleDownload(item.id);
+        handleDownload(item.url);
         
       }}
       ><Text>Télécharger</Text>
@@ -178,7 +122,8 @@ const handleDownload = ({ item }) => {
       
       </View>
       <View style={styles.carteAbonnement_body_right}>
-      <Text style={styles.carteAbonnement_header_text}>{item.nom} {item.prenom}</Text>
+      <Text style={styles.carteAbonnement_header_text}>Festival 20{item.fes_id}</Text>
+      <Text>date : {item.date_string}</Text>
       
      
       
@@ -204,10 +149,10 @@ const handleDownload = ({ item }) => {
       }}
     ><Text>Tickets</Text>
   <FlatList
-      data={Factures.Tickets}
+      data={invoices}
       // extraData={newCartes}
       renderItem={(item) => renderItem(item)}
-      keyExtractor={(item) => item.id}
+      keyExtractor={(item) => item.invoice_number}
       
       style={{ width: "100%", height: "100%" }}
       
@@ -221,40 +166,6 @@ const handleDownload = ({ item }) => {
     }
     
     ItemSeparatorComponent={() => <View style={{ height: 1, backgroundColor: "black" }} />}
-    onEndReachedThreshold={0.5}
-    refreshing={isLoading}
-    onRefresh={() => {
-      setIsLoading(true);
-      setTimeout(() => {
-        setIsLoading(false);
-      }
-      , 2000);
-      
-    }
-  }
-  
-  
-  
-  />
-
-<Text>Cartes</Text>
-  <FlatList
-      data={Factures.Cartes}
-      // extraData={newCartes}
-      renderItem={(item) => renderItem(item)}
-      keyExtractor={(item) => item.id}
-      
-      style={{ width: "100%", height: "100%" }}
-      ItemSeparatorComponent={() => <View style={{ height: 1, backgroundColor: "black" }} />}
-      onEndReached={() => {
-        setIsLoading(true);
-        setTimeout(() => {
-          setIsLoading(false);
-        }
-        , 2000);
-      }
-    }
-    
     onEndReachedThreshold={0.5}
     refreshing={isLoading}
     onRefresh={() => {
